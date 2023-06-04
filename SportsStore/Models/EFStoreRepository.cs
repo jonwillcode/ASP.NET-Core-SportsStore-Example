0e4b8265ee2a965295780c26a085019fbca6c9bd
Jonathan Williams <19499981+jonwillcode@users.noreply.github.com>
2023-06-03 20:25:43 -0400

    Add pagination

---
+++ SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsStore.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext context;
+
+        public EFStoreRepository(StoreDbContext cxt)
+        {
+            context = cxt;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
